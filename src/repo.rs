b58a953fcaeba4cd39fcd7b9b8238f08789c44ea
Tim Harder <radhermit@gmail.com>
2022-05-28 17:26:51 -0600

    repo: add comparison support for Repo objects

--- src/repo.rs
+++ src/repo.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use pkgcraft::repo;
 use pkgcraft::repo::Repository;
+use pyo3::basic::CompareOp;
 use pyo3::prelude::*;
 
 #[pyclass]
@@ -20,5 +21,16 @@ impl Repo {
 
     fn __repr__(&self) -> PyResult<String> {
         Ok(format!("<Repo '{}' at {:p}>", self.0, self))
+    }
+
+    fn __richcmp__(&self, other: PyRef<Self>, op: CompareOp) -> bool {
+        match op {
+            CompareOp::Eq => self.0 == other.0,
+            CompareOp::Ne => self.0 != other.0,
+            CompareOp::Lt => self.0 < other.0,
+            CompareOp::Gt => self.0 > other.0,
+            CompareOp::Le => self.0 <= other.0,
+            CompareOp::Ge => self.0 >= other.0,
+        }
     }
 }
--- tests/test_repo.py
+++ tests/test_repo.py
@@ -9,12 +9,14 @@ class TestRepo:
 
         # default
         repo = config.add_repo(path)
+        assert repo == config.repos[path]
         assert repo.id == path
         assert str(repo) == path
         assert repr(repo).startswith(f"<Repo '{path}' at 0x")
 
         # custom
         repo = config.add_repo(path, "fake")
+        assert repo == config.repos["fake"]
         assert repo.id == "fake"
         assert str(repo) == "fake"
         assert repr(repo).startswith(f"<Repo 'fake: {path}' at 0x")
