9fdebfece3c0f4114e1622370824d8bbf4eea7a2
Tim Harder <radhermit@gmail.com>
2022-07-15 21:27:13 -0600

    repo: minor class reordering

--- src/pkgcraft/repo/base.pyx
+++ src/pkgcraft/repo/base.pyx
@@ -4,52 +4,6 @@ from ..atom cimport Cpv
 from ..error import PkgcraftError
 
 
-cdef class _RestrictIter:
-    """Iterator that applies a restriction over a repo iterator."""
-
-    def __init__(self):  # pragma: no cover
-        raise RuntimeError(f"{self.__class__.__name__} class doesn't support manual construction")
-
-    @staticmethod
-    cdef _RestrictIter create(Repo repo, object obj):
-        cdef C.Restrict *restrict
-
-        if isinstance(obj, Cpv):
-            restrict = C.pkgcraft_atom_restrict((<Cpv>obj)._atom)
-        elif isinstance(obj, Pkg):
-            restrict = C.pkgcraft_pkg_restrict((<Pkg>obj)._pkg)
-        elif isinstance(obj, str):
-            restrict = C.pkgcraft_restrict_parse(obj.encode())
-        else:
-            raise TypeError(f"{obj.__class__.__name__!r} unsupported restriction type")
-
-        if restrict is NULL:
-            raise PkgcraftError
-
-        # create instance without calling __init__()
-        o = <_RestrictIter>_RestrictIter.__new__(_RestrictIter)
-        o._repo = repo
-        o._iter = C.pkgcraft_repo_restrict_iter(repo._repo, restrict)
-        C.pkgcraft_restrict_free(restrict)
-        return o
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        # verify __iter__() was called since cython's generated next() method doesn't check
-        if self._iter is NULL:  # pragma: no cover
-            raise TypeError(f"{self.__class__.__name__!r} object is not an iterator")
-
-        cdef C.Pkg *pkg = C.pkgcraft_repo_restrict_iter_next(self._iter)
-        if pkg is not NULL:
-            return self._repo.create_pkg(pkg)
-        raise StopIteration
-
-    def __dealloc__(self):
-        C.pkgcraft_repo_restrict_iter_free(self._iter)
-
-
 cdef class Repo:
     """Package repo."""
 
@@ -123,3 +77,49 @@ cdef class Repo:
         if not self._ref:
             C.pkgcraft_repo_free(self._repo)
         C.pkgcraft_repo_iter_free(self._repo_iter)
+
+
+cdef class _RestrictIter:
+    """Iterator that applies a restriction over a repo iterator."""
+
+    def __init__(self):  # pragma: no cover
+        raise RuntimeError(f"{self.__class__.__name__} class doesn't support manual construction")
+
+    @staticmethod
+    cdef _RestrictIter create(Repo repo, object obj):
+        cdef C.Restrict *restrict
+
+        if isinstance(obj, Cpv):
+            restrict = C.pkgcraft_atom_restrict((<Cpv>obj)._atom)
+        elif isinstance(obj, Pkg):
+            restrict = C.pkgcraft_pkg_restrict((<Pkg>obj)._pkg)
+        elif isinstance(obj, str):
+            restrict = C.pkgcraft_restrict_parse(obj.encode())
+        else:
+            raise TypeError(f"{obj.__class__.__name__!r} unsupported restriction type")
+
+        if restrict is NULL:
+            raise PkgcraftError
+
+        # create instance without calling __init__()
+        o = <_RestrictIter>_RestrictIter.__new__(_RestrictIter)
+        o._repo = repo
+        o._iter = C.pkgcraft_repo_restrict_iter(repo._repo, restrict)
+        C.pkgcraft_restrict_free(restrict)
+        return o
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # verify __iter__() was called since cython's generated next() method doesn't check
+        if self._iter is NULL:  # pragma: no cover
+            raise TypeError(f"{self.__class__.__name__!r} object is not an iterator")
+
+        cdef C.Pkg *pkg = C.pkgcraft_repo_restrict_iter_next(self._iter)
+        if pkg is not NULL:
+            return self._repo.create_pkg(pkg)
+        raise StopIteration
+
+    def __dealloc__(self):
+        C.pkgcraft_repo_restrict_iter_free(self._iter)
