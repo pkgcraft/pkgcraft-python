c1c4354b16c8598d71b23b5d78203404d60c5d00
Tim Harder <radhermit@gmail.com>
2022-08-05 21:55:39 -0600

    tests: add initial tests for load_repos_conf()

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import pytest
 
 from pkgcraft.config import Config
@@ -32,3 +34,36 @@ class TestConfig:
         # nonexistent
         with pytest.raises(PkgcraftError, match='nonexistent repo path'):
             config.add_repo_path('/path/to/nonexistent/repo')
+
+    def test_load_repos_conf(self, repo, tmp_path):
+        path = repo.path
+        config = Config()
+
+        # nonexistent
+        with pytest.raises(PkgcraftError, match='No such file or directory'):
+            config.load_repos_conf('/path/to/nonexistent/file')
+
+        # file path
+        file = tmp_path / "file"
+        file.write_text(textwrap.dedent(f"""
+            [test]
+            location = {path}
+        """))
+        config.load_repos_conf(file)
+        assert set(config.repos) == {'test'}
+
+        # dir path
+        dir_path = tmp_path / "dir"
+        dir_path.mkdir()
+        f = dir_path / "1.conf"
+        f.write_text(textwrap.dedent(f"""
+            [test1]
+            location = {path}
+        """))
+        f = dir_path / "2.conf"
+        f.write_text(textwrap.dedent(f"""
+            [test2]
+            location = {path}
+        """))
+        config.load_repos_conf(dir_path)
+        assert set(config.repos) == {'test', 'test1', 'test2'}
