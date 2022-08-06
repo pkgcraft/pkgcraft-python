f2907c0abc6529c4c294b4f161ea2424668a49da
Tim Harder <radhermit@gmail.com>
2022-08-05 22:19:10 -0600

    tests: add reload test for load_repos_conf()

--- tests/test_config.py
+++ tests/test_config.py
@@ -36,21 +36,26 @@ class TestConfig:
             config.add_repo_path('/path/to/nonexistent/repo')
 
     def test_load_repos_conf(self, repo, tmp_path):
-        path = repo.path
+        repo_path = repo.path
         config = Config()
 
         # nonexistent
-        with pytest.raises(PkgcraftError, match='No such file or directory'):
-            config.load_repos_conf('/path/to/nonexistent/file')
+        f = '/path/to/nonexistent/file'
+        with pytest.raises(PkgcraftError, match=f'config error: .* "{f}": No such file or directory'):
+            config.load_repos_conf(f)
 
         # file path
         file = tmp_path / "file"
         file.write_text(textwrap.dedent(f"""
             [test]
-            location = {path}
+            location = {repo_path}
         """))
         config.load_repos_conf(file)
         assert set(config.repos) == {'test'}
+
+        # reloading causes an existence error
+        with pytest.raises(PkgcraftError, match='config error: existing repo: test'):
+            config.load_repos_conf(file)
 
         # dir path
         dir_path = tmp_path / "dir"
@@ -58,12 +63,12 @@ class TestConfig:
         f = dir_path / "1.conf"
         f.write_text(textwrap.dedent(f"""
             [test1]
-            location = {path}
+            location = {repo_path}
         """))
         f = dir_path / "2.conf"
         f.write_text(textwrap.dedent(f"""
             [test2]
-            location = {path}
+            location = {repo_path}
         """))
         config.load_repos_conf(dir_path)
         assert set(config.repos) == {'test', 'test1', 'test2'}
