1bd299889bef71cbac6b3700c92d61051d728253
Tim Harder <radhermit@gmail.com>
2022-09-02 09:47:47 -0600

    tests: use shared test data for atom hashing

--- tests/atom/test_atom.py
+++ tests/atom/test_atom.py
@@ -112,25 +112,11 @@ class TestAtom:
             assert list(map(str, atoms)) == expected
 
     def test_hash(self):
-        for equal_versions in (
-                ('0', '0-r0', '0-r00'),
-                ('1', '01', '001'),
-                ('1.0', '1.00'),
-                ('0001.1', '1.1'),
-                ('0_beta1', '0_beta01', '0_beta001'),
-                ('1.0.2', '1.0.2-r0', '1.000.2', '1.00.2-r0'),
-                ):
-            s = {Atom(f'=cat/pkg-{x}') for x in equal_versions}
-            assert len(s) == 1
-
-        for unequal_versions in (
-                ('0', '1'),
-                ('1.01.2', '1.010.02'),
-                ('1.0', '1'),
-                ('0.1', '0.01', '0.001'),
-                ):
-            s = {Atom(f'=cat/pkg-{x}') for x in unequal_versions}
-            assert len(s) == len(unequal_versions)
+        with open(TOMLDIR / 'versions.toml', 'rb') as f:
+            d = tomli.load(f)
+        for (versions, size) in d['hashing']:
+            s = {Atom(f'=cat/pkg-{x}') for x in versions}
+            assert len(s) == size
 
     def test_cached(self):
         l = [Atom.cached('cat/pkg') for _ in range(1000)]
