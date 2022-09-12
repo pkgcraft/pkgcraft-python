9927170fa62f87b801cbb88d5b5040684bd66dc0
Tim Harder <radhermit@gmail.com>
2022-09-12 09:39:01 -0600

    restrict: split string conversion into separate function
    Also, start with Cpv and Atom tests when converting strings to
    restrictions.

--- src/pkgcraft/restrict.pyx
+++ src/pkgcraft/restrict.pyx
@@ -1,29 +1,45 @@
 from . cimport pkgcraft_c as C
-from .atom cimport Cpv
+from .atom cimport Atom, Cpv
 from .pkg cimport Pkg
-from .error import PkgcraftError
+from .error import InvalidAtom, InvalidCpv, PkgcraftError
 
 
-cdef C.Restrict *obj_to_restrict(object obj) except NULL:
-    """Convert an object to a restriction pointer."""
+cdef C.Restrict *str_to_restrict(str s) except NULL:
+    """Try to convert a string to a restriction pointer."""
     cdef C.Restrict *r
 
-    if isinstance(obj, Cpv):
-        r = C.pkgcraft_atom_restrict((<Cpv>obj)._atom)
-    elif isinstance(obj, Pkg):
-        r = C.pkgcraft_pkg_restrict((<Pkg>obj)._pkg)
-    elif isinstance(obj, str):
-        restrict_bytes = obj.encode()
-        r = C.pkgcraft_restrict_parse_dep(restrict_bytes)
-        if r is NULL:
-            r = C.pkgcraft_restrict_parse_pkg(restrict_bytes)
-    else:
-        raise TypeError(f"{obj.__class__.__name__!r} unsupported restriction type")
+    try:
+        cpv = Cpv(s)
+        return C.pkgcraft_atom_restrict(cpv._atom)
+    except InvalidCpv:
+        pass
 
+    try:
+        atom = Atom(s)
+        return C.pkgcraft_atom_restrict(cpv._atom)
+    except InvalidAtom:
+        pass
+
+    restrict_bytes = s.encode()
+    r = C.pkgcraft_restrict_parse_dep(restrict_bytes)
+    if r is NULL:
+        r = C.pkgcraft_restrict_parse_pkg(restrict_bytes)
     if r is NULL:
         raise PkgcraftError
 
     return r
+
+
+cdef C.Restrict *obj_to_restrict(object obj) except NULL:
+    """Try to convert an object to a restriction pointer."""
+    if isinstance(obj, Cpv):
+        return C.pkgcraft_atom_restrict((<Cpv>obj)._atom)
+    elif isinstance(obj, Pkg):
+        return C.pkgcraft_pkg_restrict((<Pkg>obj)._pkg)
+    elif isinstance(obj, str):
+        return str_to_restrict(obj)
+    else:
+        raise TypeError(f"{obj.__class__.__name__!r} unsupported restriction type")
 
 
 cdef class Restrict:
