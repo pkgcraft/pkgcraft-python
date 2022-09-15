1c92ea22c0bec93922f57398870cd8117ffee53c
Tim Harder <radhermit@gmail.com>
2022-09-15 15:56:56 -0600

    benches: simplify repo benchmarks for pkgcraft repo fixture support

--- benches/test_repo.py
+++ benches/test_repo.py
@@ -1,5 +1,5 @@
 import pytest
-pytest_plugins = ['pkgcraft']
+pytest_plugins = ('benchmark', 'pkgcraft')
 
 from pkgcraft.atom import Atom as pkgcraft_atom
 from pkgcore.ebuild.atom import atom as pkgcore_atom
@@ -14,16 +14,14 @@ def test_bench_repo_iter(benchmark, lib, func, repo):
         from pkgcore.ebuild import repo_objs, repository
         repo_config = repo_objs.RepoConfig(location=str(repo.path), disable_inst_caching=True)
         r = repository.UnconfiguredTree(str(repo.path), repo_config=repo_config)
+        pkgs = benchmark(lambda x: list(iter(x)), r)
     else:
-        from pkgcraft.config import Config
-        c = Config()
-        r = c.add_repo_path(repo.path)
+        pkgs = benchmark(lambda x: list(iter(x)), repo)
 
-    pkgs = benchmark(lambda x: list(iter(x)), r)
     assert len(pkgs) == 100
 
 @pytest.mark.parametrize("lib,func", (('pkgcraft', pkgcraft_atom), ('pkgcore', pkgcore_atom)))
-def test_bench_repo_iter_restrict(benchmark, lib, func, repo):
+def test_bench_repo_iter_restrict_atom(benchmark, lib, func, repo):
     # create ebuilds
     for i in range(100):
         repo.create_ebuild(f'cat/pkg-{i}')
@@ -37,10 +35,7 @@ def test_bench_repo_iter_restrict(benchmark, lib, func, repo):
         r = repository.UnconfiguredTree(str(repo.path), repo_config=repo_config)
         pkgs = benchmark(lambda x: list(r.itermatch(x)), atom)
     else:
-        from pkgcraft.config import Config
-        c = Config()
-        r = c.add_repo_path(repo.path)
-        pkgs = benchmark(lambda x: list(r.iter_restrict(x)), atom)
+        pkgs = benchmark(lambda x: list(repo.iter_restrict(x)), atom)
 
     assert len(pkgs) == 1
     assert str(pkgs[0].version) == '50'
