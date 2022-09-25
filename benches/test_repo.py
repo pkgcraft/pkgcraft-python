fbaa624cb69ddb1f6bcfa38f69c00d4c67b6ce5b
Tim Harder <radhermit@gmail.com>
2022-09-25 06:57:19 -0600

    benches: add basic RepoSet iteration benchmarks

--- benches/test_repo.py
+++ benches/test_repo.py
@@ -1,10 +1,13 @@
 import pytest
 pytest_plugins = ('benchmark', 'pkgcraft')
 
-from pkgcraft.atom import Atom as pkgcraft_atom
+from pkgcraft.config import Config
+from pkgcraft.atom import Atom
+from pkgcraft.repo import RepoSet
 from pkgcore.ebuild.atom import atom as pkgcore_atom
 
-@pytest.mark.parametrize("lib,func", (('pkgcraft', pkgcraft_atom), ('pkgcore', pkgcore_atom)))
+
+@pytest.mark.parametrize("lib,func", (('pkgcraft', Atom), ('pkgcore', pkgcore_atom)))
 def test_bench_repo_iter(benchmark, lib, func, repo):
     # create ebuilds
     for i in range(100):
@@ -20,7 +23,23 @@ def test_bench_repo_iter(benchmark, lib, func, repo):
 
     assert len(pkgs) == 100
 
-@pytest.mark.parametrize("lib,func", (('pkgcraft', pkgcraft_atom), ('pkgcore', pkgcore_atom)))
+
+def test_bench_repo_set_iter(benchmark, make_repo):
+    config = Config()
+    repo1 = make_repo(config=config)
+    repo2 = make_repo(config=config)
+    # create ebuilds
+    for i in range(50):
+        repo1.create_ebuild(f'cat/pkg-{i}')
+    for i in range(50, 100):
+        repo2.create_ebuild(f'cat/pkg-{i}')
+
+    repo = RepoSet([repo1, repo2])
+    pkgs = benchmark(lambda x: list(iter(x)), repo)
+    assert len(pkgs) == 100
+
+
+@pytest.mark.parametrize("lib,func", (('pkgcraft', Atom), ('pkgcore', pkgcore_atom)))
 def test_bench_repo_iter_restrict_atom(benchmark, lib, func, repo):
     # create ebuilds
     for i in range(100):
@@ -36,6 +55,26 @@ def test_bench_repo_iter_restrict_atom(benchmark, lib, func, repo):
         pkgs = benchmark(lambda x: list(r.itermatch(x)), atom)
     else:
         pkgs = benchmark(lambda x: list(repo.iter_restrict(x)), atom)
+
+    assert len(pkgs) == 1
+    assert str(pkgs[0].version) == '50'
+
+
+def test_bench_repo_set_iter_restrict_atom(benchmark, make_repo):
+    config = Config()
+    repo1 = make_repo(config=config)
+    repo2 = make_repo(config=config)
+    # create ebuilds
+    for i in range(50):
+        repo1.create_ebuild(f'cat/pkg-{i}')
+    for i in range(50, 100):
+        repo2.create_ebuild(f'cat/pkg-{i}')
+
+    # single atom restriction
+    atom = Atom('=cat/pkg-50')
+
+    repo = RepoSet([repo1, repo2])
+    pkgs = benchmark(lambda x: list(repo.iter_restrict(x)), atom)
 
     assert len(pkgs) == 1
     assert str(pkgs[0].version) == '50'
