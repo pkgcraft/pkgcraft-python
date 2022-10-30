f07cf39189b73458e61016667c613cce3dc1c3b3
Tim Harder <radhermit@gmail.com>
2022-10-30 07:25:40 -0600

    tests: add initial tests for FakePkg

---
+++ tests/pkg/test_fake.py
@@ -0,0 +1,40 @@
+import pytest
+
+from pkgcraft.eapi import EAPI_LATEST
+from pkgcraft.error import IndirectInit
+from pkgcraft.pkg import FakePkg
+from pkgcraft.repo import FakeRepo
+from pkgcraft.atom import Cpv, Version
+
+
+@pytest.fixture
+def repo():
+    return FakeRepo(['cat/pkg-1'], id='fake')
+
+@pytest.fixture
+def pkg(repo):
+    return next(iter(repo))
+
+
+class TestFakePkg:
+
+    def test_init(self):
+        with pytest.raises(IndirectInit):
+            FakePkg()
+
+    def test_repr(self, pkg):
+        assert repr(pkg).startswith(f"<FakePkg 'cat/pkg-1' at 0x")
+
+    def test_atom(self, pkg):
+        assert pkg.atom == Cpv('cat/pkg-1')
+
+    def test_repo(self, repo, pkg):
+        assert pkg.repo == repo
+        # repo attribute allows recursion
+        assert pkg == next(iter(pkg.repo))
+
+    def test_eapi(self, pkg):
+        assert pkg.eapi is EAPI_LATEST
+
+    def test_version(self, pkg):
+        assert pkg.version == Version('1')
