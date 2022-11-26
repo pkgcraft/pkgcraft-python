36107ed452bf068a678a01ec369dfeb04c4d9ebe
Tim Harder <radhermit@gmail.com>
2022-11-26 06:27:43 -0700

    benches: add FakeRepo specific benchmarks
    To compare with file-based EbuildRepo ones.

--- benches/test_repo.py
+++ benches/test_repo.py
@@ -7,7 +7,7 @@ from pkgcore.ebuild.atom import atom as pkgcore_atom
 
 
 @pytest.mark.parametrize("lib,func", (('pkgcraft', Atom), ('pkgcore', pkgcore_atom)))
-def test_bench_repo_iter(benchmark, lib, func, ebuild_repo):
+def test_bench_ebuild_repo_iter(benchmark, lib, func, ebuild_repo):
     # create ebuilds
     for i in range(100):
         ebuild_repo.create_ebuild(f'cat/pkg-{i}')
@@ -23,22 +23,8 @@ def test_bench_repo_iter(benchmark, lib, func, ebuild_repo):
     assert len(pkgs) == 100
 
 
-def test_bench_repo_set_iter(benchmark, make_ebuild_repo):
-    r1 = make_ebuild_repo()
-    r2 = make_ebuild_repo()
-    # create ebuilds
-    for i in range(50):
-        r1.create_ebuild(f'cat/pkg-{i}')
-    for i in range(50, 100):
-        r2.create_ebuild(f'cat/pkg-{i}')
-
-    repos = RepoSet(r1, r2)
-    pkgs = benchmark(lambda x: list(iter(x)), repos)
-    assert len(pkgs) == 100
-
-
 @pytest.mark.parametrize("lib,func", (('pkgcraft', Atom), ('pkgcore', pkgcore_atom)))
-def test_bench_repo_iter_restrict_atom(benchmark, lib, func, ebuild_repo):
+def test_bench_ebuild_repo_iter_restrict_atom(benchmark, lib, func, ebuild_repo):
     # create ebuilds
     for i in range(100):
         ebuild_repo.create_ebuild(f'cat/pkg-{i}')
@@ -56,6 +42,39 @@ def test_bench_repo_iter_restrict_atom(benchmark, lib, func, ebuild_repo):
 
     assert len(pkgs) == 1
     assert str(pkgs[0].version) == '50'
+
+
+def test_bench_fake_repo_iter(benchmark, fake_repo):
+    # create pkgs
+    fake_repo.extend([f'cat/pkg-{i}' for i in range(100)])
+    pkgs = benchmark(lambda x: list(iter(x)), fake_repo)
+    assert len(pkgs) == 100
+
+
+def test_bench_fake_repo_iter_restrict_atom(benchmark, fake_repo):
+    # create pkgs
+    fake_repo.extend([f'cat/pkg-{i}' for i in range(100)])
+
+    # single atom restriction
+    atom = Atom('=cat/pkg-50')
+
+    pkgs = benchmark(lambda x: list(fake_repo.iter_restrict(x)), atom)
+    assert len(pkgs) == 1
+    assert str(pkgs[0].version) == '50'
+
+
+def test_bench_repo_set_iter(benchmark, make_ebuild_repo):
+    r1 = make_ebuild_repo()
+    r2 = make_ebuild_repo()
+    # create ebuilds
+    for i in range(50):
+        r1.create_ebuild(f'cat/pkg-{i}')
+    for i in range(50, 100):
+        r2.create_ebuild(f'cat/pkg-{i}')
+
+    repos = RepoSet(r1, r2)
+    pkgs = benchmark(lambda x: list(iter(x)), repos)
+    assert len(pkgs) == 100
 
 
 def test_bench_repo_set_iter_restrict_atom(benchmark, make_ebuild_repo):
