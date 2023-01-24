19630fa12a5aa93991e497e118837b20ee509866
Tim Harder <radhermit@gmail.com>
2023-01-23 17:58:24 -0700

    benches: mark unused parameters as ignored
    Since they're only used for output purposes.

--- benches/test_atom.py
+++ benches/test_atom.py
@@ -22,41 +22,41 @@ atom_funcs = [
 ]
 
 
-@pytest.mark.parametrize("lib,func", atom_funcs)
-def test_bench_atom_static(benchmark, lib, func):
+@pytest.mark.parametrize("_lib,func", atom_funcs)
+def test_bench_atom_static(benchmark, _lib, func):
     benchmark(func, "=cat/pkg-1-r2:3/4=[a,b,c]")
 
 
-@pytest.mark.parametrize("lib,func", atom_funcs)
-def test_bench_atom_random(benchmark, random_str, lib, func):
+@pytest.mark.parametrize("_lib,func", atom_funcs)
+def test_bench_atom_random(benchmark, random_str, _lib, func):
     benchmark(random_atom, func, random_str)
 
 
-@pytest.mark.parametrize("lib,func", atom_funcs)
-def test_bench_atom_property(benchmark, lib, func):
+@pytest.mark.parametrize("_lib,func", atom_funcs)
+def test_bench_atom_property(benchmark, _lib, func):
     atom = func("=cat/pkg-1-r2:3/4=[a,b,c]")
     version = benchmark(getattr, atom, "version")
     assert str(version).startswith("1")
 
 
-@pytest.mark.parametrize("lib,func", atom_funcs)
-def test_bench_atom_property_none(benchmark, lib, func):
+@pytest.mark.parametrize("_lib,func", atom_funcs)
+def test_bench_atom_property_none(benchmark, _lib, func):
     atom = func("cat/pkg")
     version = benchmark(getattr, atom, "version")
     assert version is None
 
 
 # portage atoms don't natively support comparisons
-@pytest.mark.parametrize("lib,func", (("pkgcraft", pkgcraft_atom), ("pkgcore", pkgcore_atom)))
-def test_bench_atom_sorting_worst_case(benchmark, lib, func):
+@pytest.mark.parametrize("_lib,func", (("pkgcraft", pkgcraft_atom), ("pkgcore", pkgcore_atom)))
+def test_bench_atom_sorting_worst_case(benchmark, _lib, func):
     atoms = [func(f"=cat/pkg-{v}-r1:2/3=[a,b,c]") for v in reversed(range(100))]
     result = benchmark(sorted, atoms)
     assert result == list(reversed(atoms))
 
 
 # portage atoms don't natively support comparisons
-@pytest.mark.parametrize("lib,func", (("pkgcraft", pkgcraft_atom), ("pkgcore", pkgcore_atom)))
-def test_bench_atom_sorting_best_case(benchmark, lib, func):
+@pytest.mark.parametrize("_lib,func", (("pkgcraft", pkgcraft_atom), ("pkgcore", pkgcore_atom)))
+def test_bench_atom_sorting_best_case(benchmark, _lib, func):
     atoms = [func(f"=cat/pkg-{v}-r1:2/3=[a,b,c]") for v in range(100)]
     result = benchmark(sorted, atoms)
     assert result == atoms
--- benches/test_ordered_set.py
+++ benches/test_ordered_set.py
@@ -9,25 +9,25 @@ pytest_plugins = ("benchmark",)
 set_types = [("cython", CyOrderedSet), ("python", PyOrderedSet), ("standard", set)]
 
 
-@pytest.mark.parametrize("variant,cls", set_types)
-def test_bench_creation(benchmark, variant, cls):
+@pytest.mark.parametrize("_variant,cls", set_types)
+def test_bench_creation(benchmark, _variant, cls):
     s = benchmark(cls, range(100))
     assert len(s) == 100
 
 
-@pytest.mark.parametrize("variant,cls", set_types)
-def test_bench_add(benchmark, variant, cls):
+@pytest.mark.parametrize("_variant,cls", set_types)
+def test_bench_add(benchmark, _variant, cls):
     s = cls(range(100))
     benchmark(s.add, randrange(200))
 
 
-@pytest.mark.parametrize("variant,cls", set_types)
-def test_bench_discard(benchmark, variant, cls):
+@pytest.mark.parametrize("_variant,cls", set_types)
+def test_bench_discard(benchmark, _variant, cls):
     s = cls(range(1000))
     benchmark(s.discard, randrange(1000))
 
 
-@pytest.mark.parametrize("variant,cls", set_types)
-def test_bench_contains(benchmark, variant, cls):
+@pytest.mark.parametrize("_variant,cls", set_types)
+def test_bench_contains(benchmark, _variant, cls):
     s = cls(range(1000))
     benchmark(s.__contains__, randrange(1000))
