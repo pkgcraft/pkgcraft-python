dcfcd6db01406100b6ceab71bf09aed64bfc5f0a
Tim Harder <radhermit@gmail.com>
2023-01-31 03:11:02 -0700

    tests: merge Cpv tests with base module
    Matching the layout of the python modules.

--- tests/atom/test_base.py
+++ tests/atom/test_base.py
@@ -5,12 +5,45 @@ import re
 
 import pytest
 
-from pkgcraft.atom import Atom, Blocker, Cpv, SlotOperator, VersionWithOp
+from pkgcraft.atom import Atom, Blocker, Cpv, SlotOperator, Version, VersionWithOp
 from pkgcraft.eapi import EAPIS, Eapi
 from pkgcraft.error import InvalidAtom, InvalidCpv
 from pkgcraft.restrict import Restrict
 
 from ..misc import OperatorIterMap
+
+
+class TestCpv:
+    def test_init(self):
+        a = Cpv("cat/pkg-1-r2")
+        assert a.category == "cat"
+        assert a.package == "pkg"
+        assert a.version == Version("1-r2")
+        assert a.revision == "2"
+        assert a.cpn == "cat/pkg"
+        assert str(a) == "cat/pkg-1-r2"
+        assert repr(a).startswith("<Cpv 'cat/pkg-1-r2' at 0x")
+
+    def test_matches(self):
+        a = Cpv("cat/pkg-1")
+        r = Restrict(a)
+        assert a.matches(r)
+        assert not a.matches(~r)
+
+    def test_invalid(self):
+        for s in ("invalid", "cat-1", "cat/pkg", "=cat/pkg-1"):
+            with pytest.raises(InvalidCpv, match=f"invalid cpv: {s}"):
+                Cpv(s)
+
+    def test_invalid_arg_type(self):
+        for obj in (object(), None):
+            with pytest.raises(TypeError):
+                Cpv(obj)
+
+    def test_pickle(self):
+        a = Cpv("cat/pkg-1-r2")
+        b = pickle.loads(pickle.dumps(a))
+        assert a == b
 
 
 class TestBlocker:
