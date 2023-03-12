2e3426fa4a3d11b82defc98a7c9fb8f680120626
Tim Harder <radhermit@gmail.com>
2023-03-12 08:45:57 -0600

    tox: lint/reformat doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -6,15 +6,15 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath("../src/"))
 from pkgcraft.__version__ import version as pkgcraft_version
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pkgcraft'
-copyright = '2021-2023, Tim Harder'
-author = 'Tim Harder'
+project = "pkgcraft"
+copyright = "2021-2023, Tim Harder"
+author = "Tim Harder"
 version = pkgcraft_version
 release = pkgcraft_version
 
@@ -22,24 +22,24 @@ release = pkgcraft_version
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
 }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-#html_static_path = ['_static']
+html_theme = "alabaster"
+# html_static_path = ['_static']
 
 # https://alabaster.readthedocs.io/en/latest/customization.html
 html_theme_options = {
