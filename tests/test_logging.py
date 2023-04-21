fcc2ca4050bbf0b9fd815d8a80ba102168301e12
Tim Harder <radhermit@gmail.com>
2023-04-21 17:15:36 -0600

    tests: add lowered log level filtering tests

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -5,15 +5,23 @@ from pkgcraft.logging import _pkgcraft_log_test, logger
 
 def test_logging(caplog):
     for level in (logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG):
-        # verify the expected log level and message are returned
+        # verify the expected level and message are returned
         logger.setLevel(level)
         name = logging.getLevelName(level)
         _pkgcraft_log_test(name, level)
-        assert caplog.record_tuples == [("pkgcraft", level, name)], f"failed log level: {name}"
+        assert caplog.record_tuples == [("pkgcraft", level, name)], f"failed level: {name}"
         caplog.clear()
 
-        # filter log level
+        # setting a higher level filters the message
         logger.setLevel(level + 10)
         _pkgcraft_log_test(name, level)
         assert not caplog.record_tuples
         caplog.clear()
+
+        # setting a lower level passes the message, except when 0 since root
+        # loggers default to WARNING level when passed 0
+        if newLevel := level - 10:
+            logger.setLevel(newLevel)
+            _pkgcraft_log_test(name, level)
+            assert caplog.record_tuples == [("pkgcraft", level, name)], f"failed level: {name}"
+            caplog.clear()
