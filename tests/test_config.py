8f193c83b4e957b15aa84c1bcddf00bb3fa8db24
Tim Harder <radhermit@gmail.com>
2023-04-25 23:29:28 -0600

    tests: various updates for pkgcraft config changes

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,7 @@ import textwrap
 
 import pytest
 
+from pkgcraft.config import Config
 from pkgcraft.error import ConfigError, InvalidRepo
 from pkgcraft.repo import FakeRepo, RepoSet
 
@@ -22,20 +23,21 @@ class TestConfig:
         assert config.repos
         assert len(config.repos) == 1
 
-    def test_add_repo_path_ebuild(self, config, raw_ebuild_repo):
+    def test_add_repo_path_ebuild(self, raw_ebuild_repo):
         path = raw_ebuild_repo.path
 
         # default
+        config = Config()
         r = config.add_repo(path)
         assert r == config.repos[str(path)]
 
         # custom
+        config = Config()
         r = config.add_repo(path, "fake")
         assert r == config.repos["fake"]
 
-        # existing
-        with pytest.raises(ConfigError, match="existing repo: fake"):
-            config.add_repo(path, "fake")
+        # re-adding a repo doesn't fail but causes log output
+        config.add_repo(path, "fake")
 
         # nonexistent
         with pytest.raises(InvalidRepo, match="nonexistent repo path"):
@@ -72,9 +74,8 @@ class TestConfig:
         assert config.repos != {}
         assert config.repos == {"r2": r2, "r1": r1}
 
-        # re-adding a repo fails
-        with pytest.raises(ConfigError):
-            config.add_repo(r1)
+        # re-adding a repo doesn't fail but causes log output
+        config.add_repo(r1)
 
     def test_repo_sets(self, config, make_ebuild_repo, make_fake_repo):
         # empty
@@ -87,8 +88,8 @@ class TestConfig:
         assert config.repos.all == RepoSet(r1, r2)
         assert config.repos.ebuild == RepoSet(r1)
 
-    def test_load_repos_conf(self, config, raw_ebuild_repo, tmp_path):
-        repo_path = raw_ebuild_repo.path
+    def test_load_repos_conf(self, config, make_raw_ebuild_repo, tmp_path):
+        repo_path = make_raw_ebuild_repo().path
         f = tmp_path / "file"
 
         # nonexistent
@@ -130,6 +131,7 @@ class TestConfig:
             config.load_repos_conf(f)
 
         # repos.conf path default fallback
+        repo_path = make_raw_ebuild_repo(id="default").path
         f.write_text(
             textwrap.dedent(
                 f"""
@@ -142,25 +144,7 @@ class TestConfig:
         assert set(config.repos) == {"default"}
 
         # file path
-        f.write_text(
-            textwrap.dedent(
-                f"""
-            [test]
-            location = {repo_path}
-        """
-            )
-        )
-        assert list(map(str, config.load_repos_conf(f))) == ["test"]
-        assert set(config.repos) == {"default", "test"}
-
-        # reloading causes an existence error
-        with pytest.raises(ConfigError, match="config error: existing repo: test"):
-            config.load_repos_conf(f)
-
-        # dir path
-        dir_path = tmp_path / "dir"
-        dir_path.mkdir()
-        f = dir_path / "1.conf"
+        repo_path = make_raw_ebuild_repo(id="test1").path
         f.write_text(
             textwrap.dedent(
                 f"""
@@ -169,15 +153,35 @@ class TestConfig:
         """
             )
         )
+        assert list(map(str, config.load_repos_conf(f))) == ["test1"]
+        assert set(config.repos) == {"test1", "default"}
+
+        # reloading doesn't error out, but causes an existence error to be logged
+        config.load_repos_conf(f)
+
+        # dir path
+        dir_path = tmp_path / "dir"
+        dir_path.mkdir()
         f = dir_path / "2.conf"
+        r2_path = make_raw_ebuild_repo(id="test2").path
         f.write_text(
             textwrap.dedent(
                 f"""
             [test2]
-            location = {repo_path}
+            location = {r2_path}
+        """
+            )
+        )
+        f = dir_path / "3.conf"
+        r3_path = make_raw_ebuild_repo(id="test3").path
+        f.write_text(
+            textwrap.dedent(
+                f"""
+            [test3]
+            location = {r3_path}
             priority = 1
         """
             )
         )
-        assert list(map(str, config.load_repos_conf(dir_path))) == ["test2", "test1"]
-        assert set(config.repos) == {"default", "test2", "test", "test1"}
+        assert list(map(str, config.load_repos_conf(dir_path))) == ["test3", "test2"]
+        assert set(config.repos) == {"default", "test3", "test1", "test2"}
