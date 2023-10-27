515e6f500d80aead5178289d44df3ee70000dc83
Tim Harder <radhermit@gmail.com>
2023-10-27 02:49:44 -0600

    parse: drop doctests for simple validation parsing functions

--- src/pkgcraft/parse.pyx
+++ src/pkgcraft/parse.pyx
@@ -3,36 +3,21 @@ from .error import PkgcraftError
 
 
 def category(str s not None):
-    """Parse a category string.
-
-    >>> from pkgcraft import parse
-    >>> parse.category('cat')
-    True
-    """
+    """Determine if a string is a valid category name."""
     if not C.pkgcraft_parse_category(s.encode()):
         raise PkgcraftError
     return True
 
 
 def package(str s not None):
-    """Parse a package name string.
-
-    >>> from pkgcraft import parse
-    >>> parse.package('pkg')
-    True
-    """
+    """Determine if a string is a valid package name."""
     if not C.pkgcraft_parse_package(s.encode()):
         raise PkgcraftError
     return True
 
 
 def repo(str s not None):
-    """Parse a repo string.
-
-    >>> from pkgcraft import parse
-    >>> parse.repo('repo')
-    True
-    """
+    """Determine if a string is a valid repo name."""
     if not C.pkgcraft_parse_repo(s.encode()):
         raise PkgcraftError
     return True
