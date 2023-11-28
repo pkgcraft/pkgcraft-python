f39e349cbca80d300e0f5018648338d7d5939304
Tim Harder <radhermit@gmail.com>
2023-11-28 05:36:15 -0700

    benches: add basic Dep.without() and Dep.with() benchmarks where feasible

--- benches/test_dep.py
+++ benches/test_dep.py
@@ -77,3 +77,35 @@ def test_bench_dep_sorting_best_case(benchmark, _lib, func):
     deps = [func(f"=cat/pkg-{v}-r1:2/3=[a,b,c]") for v in range(100)]
     result = benchmark(sorted, deps)
     assert result == deps
+
+
+@pytest.mark.parametrize("lib", ("pkgcraft", "pkgcore", "portage"))
+def test_bench_dep_without(benchmark, lib):
+    dep = ">=cat/pkg-1-r2:3/4=::repo[a,b,c]"
+
+    match lib:
+        case "pkgcraft":
+            dep = pkgcraft_dep(dep)
+            func = lambda d: d.without("repo")
+        case "pkgcore":
+            # pkgcore doesn't have "without repo" functionality
+            dep = pkgcore_dep(dep)
+            func = lambda d: d.no_usedeps
+        case "portage":
+            dep = portage_dep(dep)
+            func = lambda d: d.without_repo
+
+    benchmark(func, dep)
+
+
+@pytest.mark.parametrize("lib", ("pkgcraft", "portage"))
+def test_bench_dep_with(benchmark, lib):
+    dep = ">=cat/pkg-1-r2:3/4=[a,b,c]"
+
+    match lib:
+        case "pkgcraft":
+            dep = pkgcraft_dep(dep)
+        case "portage":
+            dep = portage_dep(dep)
+
+    benchmark(lambda d: d.with_repo("repo"), dep)
