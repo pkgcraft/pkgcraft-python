2e928a31fdf1ce3b087f427db73de0faba0e7789
Tim Harder <radhermit@gmail.com>
2023-12-02 03:05:12 -0700

    tests: add magic method tests for UseDep objects

--- tests/dep/test_pkg.py
+++ tests/dep/test_pkg.py
@@ -58,11 +58,36 @@ class TestUseDep:
                 assert use.kind == kind
                 assert use.flag == "u"
                 assert use.missing == default
+                assert str(use) == s
+                assert s in repr(use)
 
         # invalid
         for s in ("u?(+)", "!-u"):
             with pytest.raises(PkgcraftError, match=f"invalid use dep: {re.escape(s)}"):
                 UseDep(s)
+
+    def test_eq_and_hash(self):
+        # not equal
+        u1 = UseDep("a")
+        u2 = UseDep("b")
+        assert u1 != u2
+        assert len({u1, u2}) == 2
+
+        # equal
+        u1 = UseDep("!u(-)?")
+        u2 = UseDep("!u(-)?")
+        assert u1 == u2
+        assert len({u1, u2}) == 1
+
+        # set membership
+        u = UseDep("c(+)?")
+        dep = Dep("!!>=cat/pkg-1-r2:0/2=::repo[a,-b,c(+)?]")
+        assert u in dep.use_deps
+
+        # other types
+        for obj in [None, "a/b", object()]:
+            assert u != obj
+            assert not u == obj
 
 
 class TestDep:
