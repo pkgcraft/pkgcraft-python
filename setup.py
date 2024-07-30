7b096a185da1efde6bfe54a064d5d2f35125d83a
Tim Harder <radhermit@gmail.com>
2024-07-29 21:31:05 -0600

    setup: bump pkgcraft-c version compat

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ MODULEDIR = "src/pkgcraft"
 PACKAGEDIR = os.path.dirname(MODULEDIR)
 
 # version requirements for pkgcraft C library
-MIN_VERSION = "0.0.16"
-MAX_VERSION = "0.0.16"
+MIN_VERSION = "0.0.17"
+MAX_VERSION = "0.0.17"
 
 
 def pkg_config(*packages, **kw):
